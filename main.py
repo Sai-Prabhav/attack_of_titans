81f354142b92d90a7f566317935a8fdb30091ceb
Sai-prabhav <saiprabhav.ss@gmail.com>
2022-02-08 17:19:23 +0530

    added basic code

---
+++ main.py
@@ -0,0 +1,19 @@
+import os
+import discord
+import discord.ext.commands as commands
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('TOKEN')
+client = commands.Bot(command_prefix='!a')
+
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+@client.command()
+async def ping(ctx):
+    await ctx.send(f"the ping is " + str(round(client.latency * 1000)) + "ms")
+
+client.run(TOKEN)
