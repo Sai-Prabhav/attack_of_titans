cafbe44fb25141a0e99055d3a43484a44eb243f8
Sai-prabhav <saiprabhav.ss@gmail.com>
2022-05-16 16:40:22 +0530

    fixed some bugs

--- sql_lib.py
+++ sql_lib.py
@@ -10,11 +10,11 @@ connection = psycopg2.connect(
     database="d1stpqngp1fuph", user='tawuenamkzawue', password=os.getenv("PASSWD"), host="ec2-52-214-125-106.eu-west-1.compute.amazonaws.com", port='5432'
 )
 cursor = connection.cursor()
-cursor.execute("SELECT * FROM test1")
+cursor.execute("SELECT * FROM test2")
 print(cursor.fetchall())
 
 # hard coded data
-armors = {
+armours = {
     0: {
         "id": 0,
         "name": None,
@@ -23,7 +23,7 @@ armors = {
     },
     1: {
         "id": 1,
-        "name": ["wooden armor"],
+        "name": ["wooden armour"],
         "defense": 25,
         "recipe": [
             {
@@ -38,7 +38,7 @@ armors = {
     },
     2: {
         "id": 2,
-        "name": " stone armor",
+        "name": " stone armour",
         "defense": 50,
         "recipe": [
             {
@@ -144,338 +144,28 @@ items = {
         "use": lambda user: set_hp(user.id, user.max_hp)
     }
 
-
 }
-'''
-data = {
-    "armor": [
-        {
-            "id": 0,
-            "name": null,
-            "defense": 0,
-            "recipe": null
-        },
-        {
-            "id": 1,
-            "name": "wooden armor",
-            "defense": 25,
-            "recipe": [
-                {
-                    "id": 1,  # id of stone
-                    "quantity": 15
-                },
-                {
-                    "id": 0,  # id of wood
-                    "quantity": 30
-                }
-            ]
-        },
-        {
-            "id": 2,
-            "name": " stone armor",
-            "defense": 50,
-            "recipe": [
-                {
-                    "id": 1,  # id of hard stone
-                    "quantity": 45
-                },
-                {
-                    "id": 0,  # id of hard wood
-                    "quantity": 15
-                }
-            ]
-        }
-    ],
-    "weapon": [
-        {
-            "id": 0,
-            "name": null,
-            "damage": 0,
-            "recipe": null
-        },
-        {
-            "id": 1,
-            "name": "wooden sword",
-            "damage": 10,
-            "recipe": [
-                {
-                    "id": 1,  # id of  stone
-                    "quantity": 15
-                },
-                {
-                    "id": 0,  # id of hard wood
-                    "quantity": 30
-                }
-            ]
-        },
-        {
-            "id": 2,
-            "name": " stone sword",
-            "damage": 25,
-            "recipe": [
-                {
-                    "id": 1,  # id of hard stone
-                    "quantity": 45
-                },
-                {
-                    "id": 0,  # id of wood
-                    "quantity": 15
-                }
-            ]
-        }
-    ]}
-'''
+
+quests = {
+    0: {
+        "id": 0,
+        "name": "no quest",
+        "function": None,
+        "description": None,
+        "reward": None
+    }
+}
+
 
 # functions to fetch data from database
 
 
-def isUser(id):
-    cursor.execute("SELECT id FROM test1 WHERE id = %s", (id,))
-    return cursor.fetchone() is not None
-
-
-def get_name(id):
-
-    cursor.execute("SELECT name FROM test1 WHERE id = %s", (id,))
-    return cursor.fetchone()[0]
-
-
-def get_base(id):
-
-    cursor.execute("SELECT base FROM test1 WHERE id = %s", (id,))
-    return numpy.array(cursor.fetchone()[0], dtype=int)
-
-
-def set_base(id, base):
-
-    cursor.execute("UPDATE test1 SET base = %s WHERE id = %s",
-                   (base.tolist(), id))
-    connection.commit()
-
-
-def get_armor(id):
-
-    cursor.execute("SELECT armorID FROM test1 WHERE id = %s", (id,))
-    return armour(cursor.fetchone()[-1])
-
-
-def set_armor(id, armor):
-
-    cursor.execute(
-        "UPDATE test1 SET armorID = %s WHERE id = %s", (armor.id, id))
-    connection.commit()
-
-
-def get_weapon(id):
-
-    cursor.execute("SELECT weaponID FROM test1 WHERE id = %s", (id,))
-    weapon = weapon(cursor.fetchone())
-    return weapon
-
-
-def set_weapon(id, weapon):
-
-    cursor.execute(
-        "UPDATE test1 SET weaponID = %s WHERE id = %s", (weapon.id, id))
-    connection.commit()
-
-
-def get_inventory(id):
-
-    cursor.execute("SELECT inv FROM test1 WHERE id = %s", (id,))
-    item_list = cursor.fetchone()
-    item = {}
-    print(item_list)
-    for x in item_list[0]:
-        print(x)
-        item[x[0]] = x[1]
-
-    return item
-
-
-def set_inventory(id, item):
-    lists = []
-    for keys, values in item:
-        lists.append([keys, values])
-
-    cursor.execute(
-        "UPDATE test1 SET itemID = array%s WHERE id = %s", (str(lists), id))
-    connection.commit()
-
-
-def get_level(id):
-
-    cursor.execute("SELECT level FROM test1 WHERE id = %s", (id,))
-    return cursor.fetchone()[0]
-
-
-def set_level(id, level):
-
-    cursor.execute("UPDATE test1 SET level = %s WHERE id = %s", (level, id))
-    connection.commit()
-
-
-def get_hp(id):
-
-    cursor.execute("SELECT hp FROM test1 WHERE id = %s", (id,))
-    return cursor.fetchone()[0]
-
-
-def set_hp(id, hp):
-
-    cursor.execute("UPDATE test1 SET hp = %s WHERE id = %s", (hp, id))
-    connection.commit()
-
-
-def get_money(id):
-
-    cursor.execute("SELECT money FROM test1 WHERE id = %s", (id,))
-    return cursor.fetchone()[0]
-
-
-def set_money(id, money):
-
-    cursor.execute("UPDATE test1 SET money = %s WHERE id = %s", (money, id))
-    connection.commit()
-
-
-def get_xp(id):
-
-    cursor.execute("SELECT xp FROM test1 WHERE id = %s", (id,))
-    return cursor.fetchone()[0]
-
-
-def set_xp(id, xp):
-
-    cursor.execute("UPDATE test1 SET xp = %s WHERE id = %s", (xp, id))
-    connection.commit()
-
-
-def get_strength(id):
-
-    cursor.execute("SELECT strength FROM test1 WHERE id = %s", (id,))
-    return cursor.fetchone()[0]
-
-
-def set_strength(id, strength):
-
-    cursor.execute(
-        "UPDATE test1 SET strength = %s WHERE id = %s", (strength, id))
-    connection.commit()
-
-
-def get_numb_kills(id):
-
-    cursor.execute("SELECT numb_kills FROM test1 WHERE id = %s", (id,))
-    return cursor.fetchone()[0]
-
-
-def set_numb_kills(id, numb_kills):
-
-    cursor.execute(
-        "UPDATE test1 SET numb_kills = %s WHERE id = %s", (numb_kills, id))
-    connection.commit()
-
-
-def get_numb_stone(id):
-
-    cursor.execute("SELECT numb_stones FROM test1 WHERE id = %s", (id,))
-    return cursor.fetchone()[0]
-
-
-def set_numb_stone(id, numb_stones):
-
-    cursor.execute(
-        "UPDATE test1 SET numb_stones = %s WHERE id = %s", (numb_stones, id))
-    connection.commit()
-
-
-def get_numb_wood(id):
-
-    cursor.execute("SELECT numb_wood FROM test1 WHERE id = %s", (id,))
-    return cursor.fetchone()[0]
-
-
-def set_numb_wood(id, numb_wood):
-
-    cursor.execute(
-        "UPDATE test1 SET numb_wood = %s WHERE id = %s", (numb_wood, id))
-    connection.commit()
-
-
-def get_quest(id):
-
-    cursor.execute("SELECT quest FROM test1 WHERE id = %s", (id,))
-    return quest(cursor.fetchone()[0])
-
-
-def set_quest(id, quest):
-
-    cursor.execute("UPDATE test1 SET quest = %s WHERE id = %s", (quest.id, id))
-    connection.commit()
-
-
-def get_quest_progress(id):
-
-    cursor.execute("SELECT quest_progress FROM test1 WHERE id = %s", (id,))
-    return cursor.fetchone()[0]
-
-
-def set_quest_progress(id, quest_progress):
-
-    cursor.execute(
-        "UPDATE test1 SET quest_progress = %s WHERE id = %s", (quest_progress, id))
-    connection.commit()
-
-
-class user:
-    def __init__(self, userID):
-        if not isUser(userID):
-            base = numpy.zeros([10, 10], dtype=int)  # 0 reperesent empty space
-            base[5, 5] = 1  # 1 represents the bed
-            return None
-        self.userID = userID
-        self.name = get_name(userID)
-        self.inventory = get_inventory(userID)
-        self.level = get_level(userID)
-        self.xp = get_xp(userID)
-        self.hp = get_hp(userID)
-        self.max_hp = 100+10*self.level
-        self.base = get_base(userID)
-        self.strength = get_strength(userID)
-        self.armor = get_armor(userID)
-        self.weapon = get_weapon(userID)
-        self.money = get_money(userID)
-        self.numb_kills = get_numb_kills(userID)
-        self.numb_wood = get_numb_wood(userID)
-        self.numb_stone = get_numb_stone(userID)
-        self.quest = get_quest(userID)
-        self.quest_progress = get_quest_progress(userID)
-
-    def update():
-        self.name = discord.get_user(userID).name
-        self.inventory = get_inventory(userID)
-        self.level = get_level(userID)
-        self.xp = get_xp(userID)
-        self.hp = get_hp(userID)
-        self.base = get_base(userID)
-        self.strength = get_strength(userID)
-        self.armor = get_armor(userID)
-        self.weapon = get_weapon(userID)
-        self.money = get_money(userID)
-        self.numb_kills = get_numb_kills(userID)
-        self.numb_wood = get_numb_wood(userID)
-        self.numb_stone = get_numb_stone(userID)
-        self.quest = get_quest(userID)
-        self.quest_progress = get_quest_progress(userID)
-
-
-class armor:
-    def __init__(self, armorID):
-        self.id = armorID
-        self.name = armors[armorID]["name"]
-        self.defense = armors[armorID]["defense"]
-        self.recipe = armors[armorID]["recipe"]
+class armour:
+    def __init__(self, armourID):
+        self.id = armourID
+        self.name = armours[armourID]["name"]
+        self.defense = armours[armourID]["defense"]
+        self.recipe = armours[armourID]["recipe"]
 
 
 class weapon:
@@ -504,4 +194,260 @@ class item:
         self.use = items[itemID].get("use")
 
 
+print(item(4).name)
+
+
+def isUser(id):
+    cursor.execute("SELECT id FROM test2 WHERE id = %s", (id,))
+    return cursor.fetchone() is not None
+
+
+def get_name(id):
+
+    cursor.execute("SELECT name FROM test2 WHERE id = %s", (id,))
+    return cursor.fetchone()[0]
+
+
+def get_base(id):
+
+    cursor.execute("SELECT base FROM test2 WHERE id = %s", (id,))
+    return numpy.array(cursor.fetchone()[0], dtype=int)
+
+
+def set_base(id, base):
+
+    cursor.execute("UPDATE test2 SET base = %s WHERE id = %s",
+                   (base.tolist(), id))
+    connection.commit()
+
+
+def get_armour(id):
+
+    cursor.execute("SELECT armourID FROM test2 WHERE id = %s", (id,))
+    return armour(cursor.fetchone()[-1])
+
+
+def set_armour(id, armour):
+
+    cursor.execute(
+        "UPDATE test2 SET armourID = %s WHERE id = %s", (armour.id, id))
+    connection.commit()
+
+
+def get_weapon(id):
+
+    cursor.execute("SELECT weaponID FROM test2 WHERE id = %s", (id,))
+    return weapon(cursor.fetchone()[-1])
+
+
+def set_weapon(id, weapon):
+
+    cursor.execute(
+        "UPDATE test2 SET weaponID = %s WHERE id = %s", (weapon.id, id))
+    connection.commit()
+
+
+def get_inventory(id):
+
+    cursor.execute("SELECT inv FROM test2 WHERE id = %s", (id,))
+    item_list = cursor.fetchone()
+    item = {}
+    return item
+
+
+def set_inventory(id, item):
+    lists = []
+    for keys, values in item:
+        lists.append([keys, values])
+
+    cursor.execute(
+        "UPDATE test2 SET itemID = array%s WHERE id = %s", (str(lists), id))
+    connection.commit()
+
+
+def get_level(id):
+
+    cursor.execute("SELECT level FROM test2 WHERE id = %s", (id,))
+    return cursor.fetchone()[0]
+
+
+def set_level(id, level):
+
+    cursor.execute("UPDATE test2 SET level = %s WHERE id = %s", (level, id))
+    connection.commit()
+
+
+def get_hp(id):
+
+    cursor.execute("SELECT hp FROM test2 WHERE id = %s", (id,))
+    return cursor.fetchone()[0]
+
+
+def set_hp(id, hp):
+
+    cursor.execute("UPDATE test2 SET hp = %s WHERE id = %s", (hp, id))
+    connection.commit()
+
+
+def get_money(id):
+
+    cursor.execute("SELECT money FROM test2 WHERE id = %s", (id,))
+    return cursor.fetchone()[0]
+
+
+def set_money(id, money):
+
+    cursor.execute("UPDATE test2 SET money = %s WHERE id = %s", (money, id))
+    connection.commit()
+
+
+def get_xp(id):
+
+    cursor.execute("SELECT xp FROM test2 WHERE id = %s", (id,))
+    return cursor.fetchone()[0]
+
+
+def set_xp(id, xp):
+
+    cursor.execute("UPDATE test2 SET xp = %s WHERE id = %s", (xp, id))
+    connection.commit()
+
+
+def get_strength(id):
+
+    cursor.execute("SELECT strength FROM test2 WHERE id = %s", (id,))
+    return cursor.fetchone()[0]
+
+
+def set_strength(id, strength):
+
+    cursor.execute(
+        "UPDATE test2 SET strength = %s WHERE id = %s", (strength, id))
+    connection.commit()
+
+
+def get_numb_kills(id):
+
+    cursor.execute("SELECT numb_kills FROM test2 WHERE id = %s", (id,))
+    return cursor.fetchone()[0]
+
+
+def set_numb_kills(id, numb_kills):
+
+    cursor.execute(
+        "UPDATE test2 SET numb_kills = %s WHERE id = %s", (numb_kills, id))
+    connection.commit()
+
+
+def get_numb_stone(id):
+
+    cursor.execute("SELECT stone_mine FROM test2 WHERE id = %s", (id,))
+    return cursor.fetchone()[0]
+
+
+def set_numb_stone(id, numb_stones):
+
+    cursor.execute(
+        "UPDATE test2 SET stone_mine = %s WHERE id = %s", (numb_stones, id))
+    connection.commit()
+
+
+def get_numb_wood(id):
+
+    cursor.execute("SELECT wood_chop FROM test2 WHERE id = %s", (id,))
+    return cursor.fetchone()[0]
+
+
+def set_numb_wood(id, numb_wood):
+
+    cursor.execute(
+        "UPDATE test2 SET wood_chop = %s WHERE id = %s", (numb_wood, id))
+    connection.commit()
+
+
+def get_quest(id):
+
+    cursor.execute("SELECT questid FROM test2 WHERE id = %s", (id,))
+    return quest(cursor.fetchone()[0])
+
+
+def set_quest(id, quest):
+
+    cursor.execute("UPDATE test2 SET questID = %s WHERE id = %s", (quest, id))
+    connection.commit()
+
+
+def get_quest_progress(id):
+
+    cursor.execute("SELECT quest_progress FROM test2 WHERE id = %s", (id,))
+    return cursor.fetchone()[0]
+
+
+def set_quest_progress(id, quest_progress):
+
+    cursor.execute(
+        "UPDATE test2 SET quest_progress = %s WHERE id = %s", (quest_progress, id))
+    connection.commit()
+
+
+class user:
+    def __init__(self, userID):
+        if not isUser(userID):
+            base = numpy.zeros([10, 10], dtype=int)  # 0 reperesent empty space
+            base[5, 5] = 1  # 1 represents the bed
+            return None
+        self.userID = userID
+        self.name = get_name(userID)
+        self.inventory = get_inventory(userID)
+        self.level = get_level(userID)
+        self.xp = get_xp(userID)
+        self.hp = get_hp(userID)
+        self.max_hp = 100+10*self.level
+        self.base = get_base(userID)
+        self.strength = get_strength(userID)
+        self.armour = get_armour(userID)
+        self.weapon = get_weapon(userID)
+        self.money = get_money(userID)
+        self.numb_kills = get_numb_kills(userID)
+        self.numb_wood = get_numb_wood(userID)
+        self.numb_stone = get_numb_stone(userID)
+        self.quest = get_quest(userID)
+        self.quest_progress = get_quest_progress(userID)
+
+    def __str__(self):
+        return f"""
+        Name: {self.name}
+        Level: {self.level}
+        XP: {self.xp}
+        HP: {self.hp}/{self.max_hp}
+        Strength: {self.strength}
+        Armour: {self.armour.name}
+        Weapon: {self.weapon.name}
+        Money: {self.money}
+        Number of kills: {self.numb_kills}
+        Number of wood: {self.numb_wood}
+        Number of stone: {self.numb_stone}
+        Quest: {self.quest.name}
+        Quest progress: {self.quest_progress}
+        """
+
+    def update(self):
+        self.name = discord.get_user(userID).name
+        self.inventory = get_inventory(userID)
+        self.level = get_level(userID)
+        self.xp = get_xp(userID)
+        self.hp = get_hp(userID)
+        self.base = get_base(userID)
+        self.strength = get_strength(userID)
+        self.armour = get_armour(userID)
+        self.weapon = get_weapon(userID)
+        self.money = get_money(userID)
+        self.numb_kills = get_numb_kills(userID)
+        self.numb_wood = get_numb_wood(userID)
+        self.numb_stone = get_numb_stone(userID)
+        self.quest = get_quest(userID)
+        self.quest_progress = get_quest_progress(userID)
+
+
 sai = user(512354988157103763)
+print(sai)
